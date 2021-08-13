e5fdc5e4c49e4b9347c5153986048ef2c512cdd5
BRtony <87392702+BRtony@users.noreply.github.com>
2021-08-13 09:16:24 -0300

    color changes

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 const btnMobile = document.getElementById('btn-mobile');
 
 function toggleMenu() {
-    //O toque, no mobile, normalmente tem um delay de 300ms. Isso faz com que o clique e o toque acontecam ao mesmo tempo, resultando na abertura e fechamento imediato do menu mobile. A condição abaixo impede que isso aconteça.
-    if (TouchEvent === 'touchstart') {
-        TouchEvent.preventDefault()
-    }
     const nav = document.getElementById('nav');
-    nav.classList.toggle('active'); //classList.toggle adiciona ou remove uma classe ao elemento nav. Pode ser substituído por .add ou .remove 
+    nav.classList.toggle('active'); //classList.toggle adiciona ou remove uma classe ao elemento nav. Poderia ser substituído por .add ou .remove 
     const active = nav.classList.contains('active');
-    event.currentTarget.setAttribute('aria-expanded', active);
+    this.setAttribute('aria-expanded', active);
     if (active) {
-        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+        this.setAttribute('aria-label', 'Fechar Menu'); //se a nav tem classe 'active'
     } else {
-        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
-
+        this.setAttribute('aria-label', 'Abrir Menu');
     }
 
 }
